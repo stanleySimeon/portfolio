05ee2e24fcc2d844d8b15feb25eb33c5ecd15e09
mstanley.me@gmail.com <mstanley.me@gmail.com>
2022-10-21 22:46:54 +0100

    Fix linters error

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Portfolio from './components/Portfolio';
 import Testimonial from './components/Testimonial';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import './normalize.min.css';
+import './normalize.css';
 import './index.css';
 
 export default function App() {
