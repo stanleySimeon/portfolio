387b4bfbf35c8d57d9598eca8109ab6d6a2404e2
mstanley.me@gmail.com <mstanley.me@gmail.com>
2023-02-15 18:19:12 -0500

    Update projects"

--- src/components/About.js
+++ src/components/About.js
@@ -8,20 +8,20 @@ export default function About() {
   return (
     <section id="About" className="container-fluid w-full h-auto px-4 md:px-16 lg:px-32 py-16 md:pt-32 flex flex-col justify-center items-center bg-black">
       <div className="bg md:flex justify-between md:gap-6 md:mb-12">
-        <div className="hidden wow slideInLeft md:flex about-me-picture-desktop md:w-6/12 md:h-72 md:bg-gray-600 rounded">
-          <img src={mePicture} alt="Stanley SIMEON" className="w-full h-full object-cover rounded" />
+        <div className="hidden wow slideInLeft md:block about-me-picture-desktop w-6/12 md:h-auto md:bg-myPortfolio rounded">
+          <img src={mePicture} alt="Stanley SIMEON" className="w-full h-80 object-cover rounded" />
         </div>
         <div className="md:w-full md:flex md:flex-col justify-start z-10 backdrop-blur-3xl backdrop-brightness-100 backdrop-opacity-90 p-0 md:backdrop-blur-0 md:-mt-2">
           <div className="flex flex-col justify-center md:justify-start md:items-start mb-8 md:mb-0 mt-8 md:mt-0">
             <span className="flex justify-start items-baseline text-6xl md:text-7xl font-normal md:font-bold">
-              <h1 className="wow bounceInDown text-myPortfolio-white text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold">
+              <h2 className="wow bounceInDown text-myPortfolio-white text-5xl sm:text-6xl font-bold">
                 About me
-              </h1>
+              </h2>
               <span className="text-orange-600 wow zoomInDown" data-wow-duration=".6s" data-wow-delay="1s">.</span>
             </span>
             <span className="w-32 md:hidden h-1 bg-myPortfolio-logo" />
           </div>
-          <p className="wow bounceIn text-gray-400 text-start md:text-start text-md lg:text-xl font-light mt-6 md:mt-3">
+          <p className="wow bounceIn text-gray-400 text-justify text-md lg:text-lg font-light mt-6 md:mt-3">
             <strong>Full Stack Developer</strong>
             <br />
             <span className="text-green-500 md:p-1 rounded">
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,17 +8,17 @@ export default function Contact() {
       <div className="contact__container w-full md:w-full flex flex-col md:flex-row justify-center md:justify-between md:items-start items-center md:gap-10">
         <div className="w-full md:w-10/12 lg:w-11/12 flex flex-col justify-center items-center md:items-start md:text-start text-center text-myPortfolio-white mb-10 md:gap-10">
           <div className="hidden md:flex flex-col md:flex-row md:justify-start justify-center items-center md:items-baseline md:gap-2 mb-6 md:mb-2">
-            <h1 className="wow slideInLeft text-2xl md:text-4xl text-myPortfolio-white md:font-lighter font-normal md:text-myPortfolio-logo">
+            <h2 className="wow slideInLeft text-xl uppercase text-myPortfolio-white md:font-lighter font-normal md:text-myPortfolio-logo">
               Contact
-            </h1>
+            </h2>
             <span className="wow fadeInDown bar w-12 md:w-32 h-1 bg-myPortfolio-logo md:bg-gray-600" />
           </div>
-          <h1 className="wow bounceInLeft text-5xl font-bold text-start md:text-7xl lg:text-8xl md:font-bold text-gray-200 mb-4 md:mb-2">
+          <h2 className="wow bounceInLeft text-5xl font-bold text-start sm:text-6xl md:text-7xl md:font-bold text-gray-200 mb-4 md:mb-2">
             Let&#39;s Discuss a new
             {' '}
             <span className="text-green-500">project</span>
             .
-          </h1>
+          </h2>
           <p className="text-md md:text:lg md:font-light tracking-wide hover:animate-pulse text-gray-300 text-start md:text-orange-600 md:backdrop-blur-xl p-2">
             Looking to bring your next big idea to life? Whether it&#39;s a new application, feature, or project that needs coding, you can count on me to help you make it a reality.
           </p>
