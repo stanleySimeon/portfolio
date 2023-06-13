dd8485c8d4c0271aef71d8d56241d1cb979dc3c2
Stanley SIMEON <mstanley.me@gmail.com>
2023-06-12 22:09:39 -0400

    update potfolio pages

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -23,9 +23,9 @@ const About = () => {
       <div className="flex w-full flex-col justify-center items-center">
         {
           data.map((data) => (
-            <li key={data.id} className="wow fadeInUp h-auto md:w-full lg:w-full md:flex md:justify-center border-myPortfolio-circle md:border-none md:bg-myPortfolio bg-myPortfolio-bgCard border px-2 md:px-0 pt-2 pb-6 md:mb-16 mb-10 md:gap-4 lg:gap-8 rounded-md list-none shadow-md md:odd:flex md:even:flex-row-reverse md:z-40">
+            <li key={data.id} className="wow fadeInUp h-auto md:w-full lg:w-full md:flex md:justify-center border-myPortfolio-circle md:border-none md:bg-myPortfolio bg-myPortfolio-bgCard border px-2 md:px-0 pt-2 pb-6 md:mb-16 mb-10 md:gap-4 lg:gap-8 rounded-lg list-none shadow-md md:odd:flex md:even:flex-row-reverse md:z-40">
               <div className="w-full md:w-6/12 h-full lg:w-full flex flex-col gap-2 mb-4 md:flex-col-reverse">
-                <img src={data.image} alt={data.title} className="rounded md:border md:border-myPortfolio-circle" id="Resize" />
+                <img src={data.image} alt={data.title} className="rounded-lg md:border md:border-myPortfolio-circle" id="Resize" />
                 <div className="md:flex md:justify-start md:items-baseline md:gap-2">
                   <span className="bar hidden md:flex md:w-8 lg:w-12 bg-myPortfolio-white" />
                   <h2 className="text-myPortfolio-white md:text-myPortfolio-secondaryText text-lg md:text-2xl font-bold uppercase">{data.title}</h2>
@@ -35,7 +35,7 @@ const About = () => {
                 <p className="text-gray-400 text-md md:text-lg font-light md:mt-8 lg:mt-2">{data.description}</p>
                 <span className="hidden md:flex gap-2 md:mt-6">
                   {data.technologies.map((tech) => (
-                    <span key={tech} className="text-myPortfolio-white text-sm lg:text-md md:px-1 lg:px-2 font-normal bg-myPortfolio-logo rounded-sm wow bounceInUp">{tech}</span>
+                    <span key={tech} className="text-myPortfolio-white text-sm lg:text-md md:px-1 lg:px-2 py-1 font-normal bg-myPortfolio-logo rounded-md">{tech}</span>
                   ))}
                 </span>
                 <div className="flex justify-center items-center md:justify-start gap-4 mt-4 md:mt-6 font-normal">
